a3d671d7f57721ee0bcd974b844072ead378377f
goldingj <jgolding.jgo@gmail.com>
2026-06-22 03:45:43 -0400

    updates CORS policy

--- server/Program.cs
+++ server/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact", policy =>
     {
-        policy.WithOrigins("http://localhost:5173")
+        policy.WithOrigins("http://localhost:5173", "https://working-branch.d2qtozm702yx16.amplifyapp.com/")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
